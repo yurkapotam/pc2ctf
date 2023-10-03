a995f843ab9b93b173ca783a57a025e4464830bf
yurkapotam <111305715+yurkapotam@users.noreply.github.com>
2023-10-03 17:29:24 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
 Damage.FriendlyFire = GameMode.Parameters.GetBool("FriendlyFire");
+
+var RedFlagCarried = false;
+var RedFlagCarrier = ":D";
+
+var BlueFlagCarried = false;
+var BlueFragCarrier = ":)";
+
+Damage.OnDeath.Add(function(ply) {
+    if (ply.IdInRoom == RedFlagCarrier)
+    {
+        RedFlagCarried = false;
+    }
+    else if (ply.IdInRoom == BlueFlagCarrier)
+    {
+        BlueFlagCarried = false;
+    }
+});
