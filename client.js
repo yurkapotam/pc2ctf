fcf0853f53a5afaddfa6e4a29c98358272143141
yurkapotam <111305715+yurkapotam@users.noreply.github.com>
2023-10-04 13:13:15 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,27 +1,12 @@
 Damage.FriendlyFire = GameMode.Parameters.GetBool("FriendlyFire");
 
-var RedFlagCarried = false;
-var RedFlagCarrier = ":D";
 Teams.Add("Red", "Teams/Red", {r: 1});
 var red = Teams.Get("Red");
 red.Spawns.SpawnPointsGroups.Add(2);
 
-var BlueFlagCarried = false;
-var BlueFragCarrier = ":)";
 Teams.Add("Blue", "Teams/Blue", {b: 1});
 var blue = Teams.Get("Blue");
 blue.Spawns.SpawnPointsGroups.Add(1);
-
-Damage.OnDeath.Add(function(ply) {
-    if (ply.IdInRoom == RedFlagCarrier && RedFlagCarried)
-    {
-        RedFlagCarried = false;
-    }
-    else if (ply.IdInRoom == BlueFlagCarrier && BlueFlagCarried)
-    {
-        BlueFlagCarried = false;
-    }
-});
 
 Teams.OnRequestJoinTeam.Add(function(player, team) {
     team.Add(player);
