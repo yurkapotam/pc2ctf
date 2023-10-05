367cfefe2a0a3956ddcb4d7f457765a839291b8a
yurkapotam <111305715+yurkapotam@users.noreply.github.com>
2023-10-05 14:15:01 +0300

    Update client.js

--- client.js
+++ client.js
@@ -46,9 +46,42 @@ var defView = AreaViewService.GetContext().Get("BlueFlagView");
 defView.color={b:1};
 defView.Enable = true;
 
-// создаем триггер зон защиты
 var defTrigger = AreaPlayerTriggerService.Get("BlueFlagTrigger");
 defTrigger.OnEnter.Add(function(player) {
-    player.Properties.Get("FlagCarrier").Value = true;
+    if (player.Team == red)
+    {
+        player.Properties.Get("FlagCarrier").Value = true;
+        defTrigger.Enable = false;
+        defView.Enable = false;
+        Ui.GetContext().Hint.Value = "Синий флаг захватили!\nНоситель: " + player.NickName;
+    }
+    else if (player.Team == blue && player.Properties.Get("FlagCarrier").Value == true)
+    {
+        player.Properties.Get("FlagCarrier").Value = false;
+        defiTrigger.Enable = true;
+        defiView.Enable = true;
+    }
 });
 defTrigger.Enable = true;
+
+var defiView = AreaViewService.GetContext().Get("BlueFlagView");
+defiView.color={b:1};
+defiView.Enable = true;
+
+var defiTrigger = AreaPlayerTriggerService.Get("BlueFlagTrigger");
+defiTrigger.OnEnter.Add(function(player) {
+    if (player.Team == blue)
+    {
+        player.Properties.Get("FlagCarrier").Value = true;
+        defiTrigger.Enable = false;
+        defiView.Enable = false;
+        Ui.GetContext().Hint.Value = "Красный флаг захватили!\nНоситель: " + player.NickName;
+    }
+    else if (player.Team == red && player.Properties.Get("FlagCarrier").Value == true)
+    {
+        player.Properties.Get("FlagCarrier").Value = false;
+        defTrigger.Enable = true;
+        defView.Enable = true;
+    }
+});
+defiTrigger.Enable = true;
